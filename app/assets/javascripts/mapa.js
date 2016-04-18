6eb805d4ee417047151966226828bc75f798e8a4
jaimecartodb <jdemora@cartodb.com>
2016-04-18 18:31:57 +0200

    first version working

--- app/assets/javascripts/mapa.js
+++ app/assets/javascripts/mapa.js
@@ -104,7 +104,7 @@ $( document ).on('ready', function(){
   // L.control.layers(osm, dark_matter).addTo(map);
 
 
-  map.locate({setView:true, watch:false, maxZoom: 13})
+  map.locate({setView:true, watch:false, maxZoom: 14})
   .on('locationfound', function(l){
     //var pos=l.latlng;
     console.log(l);
@@ -137,48 +137,55 @@ $( document ).on('ready', function(){
      	},
      	'bus_path': {
      	  sql: 'SELECT * FROM bus',
-     		cartocss: '#bus{line-color: #FF6600; line-width: 2; line-opacity: 0.7;}'
+     		cartocss: '#bus{line-color: #FF6600; line-width: 3; line-opacity: 0.7;}'
      	},
       'traffic_cameras': {
         sql: 'SELECT * FROM camaras',
-        cartocss: '#camaras{marker-fill-opacity: 0.9; marker-fill: #FF6600; marker-allow-overlap: true;}'
+        cartocss: '#camaras{marker-file: url(http://com.cartodb.users-assets.production.s3.amazonaws.com/simpleicon/map50.svg); marker-fill-opacity: 0.9; marker-line-color: #FFF; marker-line-width: 1; marker-line-opacity: 1; marker-placement: point; marker-type: ellipse; marker-width: 11; marker-fill: #000000; marker-allow-overlap: true; }'
       },
       'traffic_incidents': {
         sql: 'SELECT * FROM incidentes_trafico',
-        cartocss: '#incidentes_trafico{marker-fill-opacity: 0.9; marker-fill: #0080ff; marker-allow-overlap: true;}'
+        cartocss: '#incidentes_trafico{marker-fill-opacity: 0.9; marker-line-color: #FFF; marker-line-width: 1; marker-line-opacity: 1; marker-placement: point; marker-type: ellipse; marker-width: 13.5; marker-fill: #B81609; marker-allow-overlap: true; }'
       },
       'train_stations': {
         sql: 'SELECT * FROM estaciones_tren',
-        cartocss: '#estaciones_tren{ marker-fill-opacity: 0.9; marker-fill: #FF6600;}'
-      },
+        cartocss: '#estaciones_tren{marker-fill-opacity: 0.9; marker-line-color: #850200; marker-line-width: 1; marker-line-opacity: 0.8; marker-placement: point; marker-type: ellipse; marker-width: 12; marker-fill: #FFFFFF; marker-allow-overlap: true; }'},
       'urban_buses': {
         sql: 'SELECT * FROM autobuses_urbanos',
-        cartocss: '#autobuses_urbanos{ marker-fill-opacity: 0.9; marker-fill: #7B00B4;}'
+        cartocss: '#autobuses_urbanos{marker-fill-opacity: 1; marker-line-color: #FFF; marker-line-width: 1; marker-line-opacity: 0.8; marker-placement: point; marker-type: ellipse; marker-width: 13; marker-fill: #D6301D; marker-allow-overlap: true; }'
       },
       'interurban_buses': {
         sql: 'SELECT * FROM autobuses_interurbanos',
-        cartocss: '#autobuses_interurbanos{ marker-fill-opacity: 0.9; marker-fill: #000000;}'
+        cartocss: '#autobuses_interurbanos{marker-fill-opacity: 0.9; marker-line-color: #FFF; marker-line-width: 1; marker-line-opacity: 0.4; marker-placement: point; marker-type: ellipse; marker-width: 10; marker-fill: #055D00; marker-allow-overlap: true; }'
       },
       'metro_stops': {
         sql: 'SELECT * FROM paradas_metro',
-        cartocss: '#paradas_metro{ marker-fill-opacity: 0.9; marker-fill: #FFCC00;}'
-      },
+        cartocss: '#paradas_metro{marker-file: url(http://com.cartodb.users-assets.production.s3.amazonaws.com/pin-maps/location10.svg); marker-fill-opacity: 0.9; marker-line-color: #FFF; marker-line-width: 1; marker-line-opacity: 1; marker-placement: point; marker-type: ellipse; marker-width: 11.5; marker-fill: #D6301D; marker-allow-overlap: true; }'},
       'light_metro_stops': {
         sql: 'SELECT * FROM paradas_metro_ligero',
         cartocss: '#paradas_metro_ligero{ marker-fill-opacity: 0.9; marker-fill: #FF6600;}'
       },
       'taxi_stops': {
         sql: 'SELECT * FROM paradas_taxi',
-        cartocss: '#paradas_taxi{ marker-fill-opacity: 0.9; marker-fill: #055D00;}',
-        interactivity: 'cartodb_id, name'
-      },
+        cartocss: '#paradas_taxi{marker-fill-opacity: 0.9; marker-line-color: #FFF; marker-line-width: 1; marker-line-opacity: 0.7; marker-placement: point; marker-type: ellipse; marker-width: 13; marker-fill: #3E7BB6; marker-allow-overlap: true; }', interactivity: 'cartodb_id, name'
+      }, 
       'madrid_neighbours': {
         sql: 'SELECT * FROM madrid_barrios_parametros',
-        cartocss: '#madrid_barrios_parametros{polygon-fill: #FFFFB2; polygon-opacity: 0.8; line-color: #FFF; line-width: 0.5; line-opacity: 1; } #madrid_barrios_parametros [ seguridad <= 5] { polygon-fill: #B10026; } #madrid_barrios_parametros [ seguridad <= 5] { polygon-fill: #E31A1C; } #madrid_barrios_parametros [ seguridad <= 4] {   polygon-fill: #FC4E2A; } #madrid_barrios_parametros [ seguridad <= 4] {   polygon-fill: #FD8D3C; } #madrid_barrios_parametros [ seguridad <= 3] { polygon-fill: #FEB24C; } #madrid_barrios_parametros [ seguridad <= 3] {   polygon-fill: #FED976; } #madrid_barrios_parametros [ seguridad <= 2] {    polygon-fill: #FFFFB2; }'
+        cartocss: '#madrid_barrios_parametros{polygon-fill: #B10026; polygon-opacity: 0.8; line-color: #055D00; line-width: 1; line-opacity: 0.4; } #madrid_barrios_parametros::labels {text-name: [barrio]; text-face-name: "DejaVu Sans Book"; text-size: 12; text-label-position-tolerance: 10; text-fill: #0F3B82; text-halo-fill: #FFF; text-halo-radius: 0.0; text-dy: -12; text-allow-overlap: false; text-placement: interior; text-placement-type: simple; } #madrid_barrios_parametros [ seguridad <= 5] {polygon-fill: #FFFFB2; } #madrid_barrios_parametros [ seguridad <= 5] {polygon-fill: #FED976; } #madrid_barrios_parametros [ seguridad <= 4] {polygon-fill: #FEB24C; } #madrid_barrios_parametros [ seguridad <= 4] {polygon-fill: #FD8D3C; } #madrid_barrios_parametros [ seguridad <= 3] {polygon-fill: #FC4E2A; } #madrid_barrios_parametros [ seguridad <= 3] {polygon-fill: #E31A1C; } #madrid_barrios_parametros [ seguridad <= 2] {polygon-fill: #B10026; }'
+        },
+
+      'madrid_leisure': {
+        sql: 'SELECT * FROM madrid_barrios_parametros',
+        cartocss: '#madrid_barrios_parametros{ polygon-fill: #1a9850; polygon-opacity: 0.8; line-color: #055D00; line-width: 1; line-opacity: 0.2; } #madrid_barrios_parametros::labels {text-name: [barrio]; text-face-name: "DejaVu Sans Book"; text-size: 10; text-label-position-tolerance: 10; text-fill: #0F3B82; text-halo-fill: #FFF; text-halo-radius: 0; text-dy: -12; text-allow-overlap: false; text-placement: interior; text-placement-type: simple; } #madrid_barrios_parametros [ bares_y_re <= 5] {polygon-fill: #d73027; } #madrid_barrios_parametros [ bares_y_re <= 5] {polygon-fill: #f79272; } #madrid_barrios_parametros [ bares_y_re <= 4] {polygon-fill: #fed6b0; } #madrid_barrios_parametros [ bares_y_re <= 4] {polygon-fill: #fff2cc; } #madrid_barrios_parametros [ bares_y_re <= 3] {polygon-fill: #d2ecb4; } #madrid_barrios_parametros [ bares_y_re <= 3] {polygon-fill: #8cce8a; } #madrid_barrios_parametros [ bares_y_re <= 2] {polygon-fill: #1a9850; }' 
+      },
+
+      'madrid_clean': {
+        sql: 'SELECT * FROM madrid_barrios_parametros',
+        cartocss: '#madrid_barrios_parametros{polygon-fill: #005824; polygon-opacity: 0.8; line-color: #055D00; line-width: 1; line-opacity: 0.4; } #madrid_barrios_parametros::labels {text-name: [barrio]; text-face-name: "DejaVu Sans Book; text-size: 10; text-label-position-tolerance: 10; text-fill: #0F3B82; text-halo-fill: #FFF; text-halo-radius: 0; text-dy: -12; text-allow-overlap: false; text-placement: interior; text-placement-type: simple; } #madrid_barrios_parametros [ limpieza <= 5] {polygon-fill: #EDF8FB; } #madrid_barrios_parametros [ limpieza <= 5] {polygon-fill: #D7FAF4; } #madrid_barrios_parametros [ limpieza <= 4] {polygon-fill: #CCECE6; } #madrid_barrios_parametros [ limpieza <= 4] {polygon-fill: #66C2A4; } #madrid_barrios_parametros [ limpieza <= 3] {polygon-fill: #41AE76; } #madrid_barrios_parametros [ limpieza <= 3] {polygon-fill: #238B45; } #madrid_barrios_parametros [ limpieza <= 2] {polygon-fill: #005824; }'
         },
       'madrid_neighbours_boundaries': {
         sql: 'SELECT * FROM madrid_barrios_parametros',
-        cartocss: '#madrid_barrios_parametros{polygon-fill: #FFFFB2; polygon-opacity: 0; line-color: #055D00; line-width: 1; line-opacity: 1; } #madrid_barrios_parametros::labels {text-name: [barrio]; text-face-name: "DejaVu Sans Book"; text-size: 12; text-label-position-tolerance: 10; text-fill: #0F3B82; text-halo-fill: #FFF; text-halo-radius: 0.5; text-dy: -15; text-allow-overlap: false; text-placement: interior; text-placement-type: simple; } #madrid_barrios_parametros [ seguridad <= 5] {polygon-fill: #B10026; } #madrid_barrios_parametros [ seguridad <= 5] {polygon-fill: #E31A1C; } #madrid_barrios_parametros [ seguridad <= 4] {polygon-fill: #FC4E2A; } #madrid_barrios_parametros [ seguridad <= 4] {polygon-fill: #FD8D3C; } #madrid_barrios_parametros [ seguridad <= 3] {polygon-fill: #FEB24C; } #madrid_barrios_parametros [ seguridad <= 3] {polygon-fill: #FED976; } #madrid_barrios_parametros [ seguridad <= 2] {polygon-fill: #FFFFB2; }'}
+        cartocss: '#madrid_barrios_parametros{polygon-fill: #FFFFB2; polygon-opacity: 0; line-color: #055D00; line-width: 1; line-opacity: 1; } #madrid_barrios_parametros::labels {text-name: [barrio]; text-face-name: "DejaVu Sans Book"; text-size: 12; text-label-position-tolerance: 10; text-fill: #0F3B82; text-halo-fill: #FFF; text-halo-radius: 0.1; text-dy: -15; text-allow-overlap: false; text-placement: interior; text-placement-type: simple; } #madrid_barrios_parametros [ seguridad <= 5] {polygon-fill: #B10026; } #madrid_barrios_parametros [ seguridad <= 5] {polygon-fill: #E31A1C; } #madrid_barrios_parametros [ seguridad <= 4] {polygon-fill: #FC4E2A; } #madrid_barrios_parametros [ seguridad <= 4] {polygon-fill: #FD8D3C; } #madrid_barrios_parametros [ seguridad <= 3] {polygon-fill: #FEB24C; } #madrid_barrios_parametros [ seguridad <= 3] {polygon-fill: #FED976; } #madrid_barrios_parametros [ seguridad <= 2] {polygon-fill: #FFFFB2; }'}
     };
 
     var columns = {
@@ -195,22 +202,21 @@ $( document ).on('ready', function(){
       'madrid_neighbours': ['cartodb_id', 'barrio']
     }
 
-//   var coolTemplate = '<script type="infowindow/html" id="infowindow_template">
-//   <div class="cartodb-popup v2">
-//     <a href="#close" class="cartodb-popup-close-button close">x</a>
-//      <div class="cartodb-popup-content-wrapper">
-//        <div class="cartodb-popup-header">
-//          <img style="width: 100%" src="http://cartodb.com/assets/logos/logos_full_cartodb_light.png"></src>
-//        </div>
-//        <div class="cartodb-popup-content">
-//          <!-- content.data contains the field info -->
-//          <h4>City: </h4>
-//          <p>{{content.data.name}}</p>
-//        </div>
-//      </div>
-//      <div class="cartodb-popup-tip-container"></div>
-//   </div>
-// </script>';
+//   var coolTemplate = '
+  // <div class="cartodb-popup v2">
+  //   <a href="#close" class="cartodb-popup-close-button close">x</a>
+  //    <div class="cartodb-popup-content-wrapper">
+  //      <div class="cartodb-popup-header">
+  //        <img style="width: 100%" src="http://cartodb.com/assets/logos/logos_full_cartodb_light.png"></src>
+  //      </div>
+  //      <div class="cartodb-popup-content">
+  //        <!-- content.data contains the field info -->
+  //        <h4>City: </h4>
+  //        <p>{{content.data.name}}</p>
+  //      </div>
+  //    </div>
+  //    <div class="cartodb-popup-tip-container"></div>
+  // </div>';
 
     // $(function() {
     //        $( "#slider-range-min" ).slider({
@@ -251,7 +257,7 @@ $( document ).on('ready', function(){
           var numSubLayers = layer.getSubLayerCount();
           console.log(columns[$(this).attr("id")]);
           cdb.vis.Vis.addInfowindow(map, layer.getSubLayer(numSubLayers-1), columns[$(this).attr("id")], {
-            infowindowTemplate: '<p>{{name}}</p>'
+            infowindowTemplate: '<div class="cartodb-popup v2"> <a href="#close" class="cartodb-popup-close-button close">x</a> <div class="cartodb-popup-content-wrapper"> <div class="cartodb-popup-header"> <img style="width: 100%" src="http://www.vectorlogo.es/wp-content/uploads/2014/12/ayuntamiento-madrid-logo-vector.jpg"></src> </div> <div class="cartodb-popup-content"> <!-- content.data contains the field info --> <h4>City: </h4> <p>{{content.data.name}}</p> </div> </div> <div class="cartodb-popup-tip-container"></div> </div>'
           });
      		});
         window.cdb_layer.getSubLayers().forEach(function(l){
@@ -304,10 +310,14 @@ $( document ).on('ready', function(){
         window.cdb_layer.getSubLayers().forEach(function(l){
           var sql = l.sql_origin + " WHERE ST_INTERSECTS(the_geom, st_geomfromtext('" + getPolWKT(e.layer) + "', 4326))";
           l.setSQL(sql);
+          $('#download').click(function(){
+          window.open("https://jaimedemora.cartodb.com/api/v2/sql?format=GeoJSON&q=" + sql + "")
+        });
         });
         // if (type === 'marker') {
         //   layerdrawn.bindPopup('A popup!');
         // }
+        
         drawnItems.getLayers().forEach(function(d){
           drawnItems.removeLayer(d);
         });
